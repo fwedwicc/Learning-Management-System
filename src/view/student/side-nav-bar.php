be0ee93e7753cbb10f1aafa42d3b19a1e2fcf6ff
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-04-05 17:40:10 +0800

    Finished the course overview for student

--- src/view/student/side-nav-bar.php
+++ src/view/student/side-nav-bar.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Learning Management System | Login</title>
+  <title>Learning Management System | Student Layout</title>
   <!-- Tailwind cdn -->
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
