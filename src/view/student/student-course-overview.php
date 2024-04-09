e9a6922a1d43c6fafd4c6d1179ba5600aaabfd5b
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-04-09 11:37:31 +0800

    Added new file for student material/PDF also added dummy PDF for content

--- src/view/student/student-course-overview.php
+++ src/view/student/student-course-overview.php
@@ -116,7 +116,7 @@ include_once './side-nav-bar.php';
                 <h3 class="text-lg font-semibold text-deep-koamaru">Learning Materials</h3>
                 <ul class="list-none pt-4 text-gray-500 dark:text-gray-400 space-y-3">
                   <li>
-                    <a href="#" class="w-full px-3 py-2 text-sm font-medium text-center inline-flex items-center text-royal-blue border border-royal-blue hover:bg-hawkes-blue focus:ring-4 focus:outline-none focus:ring-tropical-blue rounded-lg text-sm bg-solitude transition ease-in-out duration-300">
+                    <a href="./student-material.php" class="w-full px-3 py-2 text-sm font-medium text-center inline-flex items-center text-royal-blue border border-royal-blue hover:bg-hawkes-blue focus:ring-4 focus:outline-none focus:ring-tropical-blue rounded-lg text-sm bg-solitude transition ease-in-out duration-300">
                       <svg class="w-3 h-3 me-2 text-royal-blue" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2c.6 0 1-.4 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clip-rule="evenodd" />
                       </svg>
